f8385db42f660f2dca324fcc9ab389133a3d527b
Darius <133234590+dariussindrilaru@users.noreply.github.com>
2023-12-01 09:14:09 +0200

    added 16x16 grid

---
+++ script.js
@@ -0,0 +1,21 @@
+let rows = 16;
+let columns = 16;
+let gridSize = 600;
+const container = document.querySelector('#container');
+container.style.width = `${gridSize}px`;
+container.style.height = `${gridSize}px`;
+
+
+
+function createGrid() {
+    for (let i = 0; i < (rows * columns); i++) {
+        const gridBox = document.createElement('div');
+        // gridBox.textContent = i + 1;
+        gridBox.style.width = `${gridSize / columns}px`;
+        gridBox.style.height = `${gridSize / rows}px`;
+        gridBox.classList.add('grid-box');
+
+        container.appendChild(gridBox);
+    }
+}
+createGrid();
