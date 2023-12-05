ccd17f70395b01e862c5efa5051fb77b0638d588
Darius <133234590+dariussindrilaru@users.noreply.github.com>
2023-12-05 20:47:49 +0200

    added a button for changing the grid size using the prompt method

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 let rows = 16;
 let columns = 16;
-let gridSize = 600;
+let gridSize = 750;
+
+const changeSize = document.querySelector('.change-size');
 const container = document.querySelector('#container');
 container.style.width = `${gridSize}px`;
 container.style.height = `${gridSize}px`;
 
-
 function createGrid() {
+    container.textContent = '';
     for (let i = 0; i < (rows * columns); i++) {
         const gridBox = document.createElement('div');
-        // gridBox.textContent = i + 1;
 
         gridBox.style.width = `${gridSize / columns}px`;
         gridBox.style.height = `${gridSize / rows}px`;
@@ -24,3 +25,15 @@ function createGrid() {
 }
 
 createGrid();
+
+changeSize.addEventListener('click', () => {
+    let userSize = Number(prompt('Enter a number of squares per side for the new grid (up to 100):'));
+    if (userSize > 0 && userSize <= 100) {
+        rows = userSize;
+        columns = userSize;
+    } else {
+        alert('Please enter a valid number between 1 and 100.');
+    }
+
+    createGrid();
+});
